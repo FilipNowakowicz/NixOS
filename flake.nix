b293e62f49e743b1f18fd9ccf6c5f2c4faf91035
Filip Nowakowicz <48061679+FilipNowakowicz@users.noreply.github.com>
2025-12-08 03:33:07 +0000

    Populate flake configuration

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "NixOS and Home Manager configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8ZsS6UqK+KP5gdiQNRrt65TUI="
+    ];
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      mkHost = name:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/${name}.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+          ];
+        };
+    in {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ nixd nil deploy-rs ];
+      };
+
+      nixosConfigurations = {
+        main-vm = mkHost "main-vm";
+        labvm = mkHost "labvm";
+        watchonly = mkHost "watchonly";
+        coldvm = mkHost "coldvm";
+        laptop = mkHost "laptop";
+      };
+    };
+}
