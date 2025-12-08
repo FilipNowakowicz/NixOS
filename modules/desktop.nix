5468c4b096a422f5b9cf36c695f66c4af1b8e2b7
Filip Nowakowicz <48061679+FilipNowakowicz@users.noreply.github.com>
2025-12-08 03:35:01 +0000

    Add initial system modules

---
+++ modules/desktop.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+{
+  services = {
+    xserver = {
+      enable = true;
+      layout = "us";
+      xkbVariant = "";
+      xkbOptions = "caps:escape";
+
+      desktopManager.xterm.enable = false;
+      displayManager.gdm = {
+        enable = true;
+        wayland = true;
+      };
+
+      windowManager.awesome.enable = true;
+    };
+
+    xwayland.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+  };
+
+  hardware.pulseaudio.enable = false;
+
+  programs = {
+    dconf.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    awesome
+    gnome.gnome-themes-extra
+    xdg-desktop-portal-gtk
+  ];
+
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      dejavu_fonts
+      liberation_ttf
+    ];
+  };
+}
